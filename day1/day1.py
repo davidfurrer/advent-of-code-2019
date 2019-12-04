a253044bb58e7dd65b90f3e27c42f85633ef3797
fulowa <fulowa@gmail.com>
2019-12-04 15:36:59 +0100

    day1 part2

--- day1/day1.py
+++ day1/day1.py
@@ -11,4 +11,16 @@ def get_fuel_required(x):
 df["fuel_required"] = df["mass"].apply(lambda x: get_fuel_required(x))
 
 total_fuel_required = df["fuel_required"].sum()
-print(total_fuel_required)
+print(f"Part 1: {total_fuel_required}")
+
+
+additional_fuel = 0
+for fuel in df["fuel_required"]:
+    fuel_ = fuel
+    while get_fuel_required(fuel_) > 0:
+        additional_fuel += get_fuel_required(fuel_)
+        fuel_ = get_fuel_required(fuel_)
+
+
+print(f"Part 2: {total_fuel_required + additional_fuel}")
+
