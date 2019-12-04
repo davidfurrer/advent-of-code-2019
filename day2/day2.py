ac045eac35d3a8f40f775d90dc86c36ae6fd6778
fulowa <fulowa@gmail.com>
2019-12-04 17:49:04 +0100

    d2.2

--- day2/day2.py
+++ day2/day2.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import tqdm
 
 df = pd.read_csv("day2/input.txt", header=None).T
-df.columns = ['p']
-pl = df.p.tolist()
+df.columns = ["p"]
+pl = df.p.tolist().copy()
 
-print(pl)
 # 0 = add
 # 1 = multiply
 # 99 = stop
@@ -13,21 +13,35 @@ pl[1] = 12
 pl[2] = 2
 
 
-for i, j in enumerate(pl):
-    print(i, j)
-    if i % 4 == 0:
-        print(i)
-        if j == 1:
-            ii = pl[i + 3]
-            pl[ii] = pl[pl[i+1]] + pl[pl[i+2]]
-        elif j == 2:
-            ii = pl[i + 3]
-            pl[ii] = pl[pl[i+1]] * pl[pl[i+2]]
-        elif df.loc[i, "p"] == 99:
-            print("break 99")
-            break
-        else:
-            print("break unknown")
+def run_program(im, jm):
+    pl[1] = im
+    pl[2] = jm
+    for i, j in enumerate(pl):
+        if i % 4 == 0:
+            if j == 1:
+                ii_ = pl[i + 3]
+                pl[ii_] = pl[pl[i + 1]] + pl[pl[i + 2]]
+            elif j == 2:
+                ii_ = pl[i + 3]
+                pl[ii_] = pl[pl[i + 1]] * pl[pl[i + 2]]
+            elif j == 99:
+                return pl[0]
+                break
+            else:
+                return pl[0]
+                break
+    return pl[0]
+
+
+print("result part1:", run_program(12, 2))
+
+result = 19690720
+for ii in tqdm.tqdm(range(100)):
+    for jj in range(100):
+        pl = df.p.tolist().copy()
+        result_ = run_program(ii, jj)
+        if result == result_:
+            r = 100 * ii + jj
+            print("result part2:", r)
             break
 
-print('result part1:', pl[0])
