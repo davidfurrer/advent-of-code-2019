cdc15647063b88cbded99c5433d6c40821bf32d9
fulowa <fulowa@gmail.com>
2019-12-04 15:09:36 +0100

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,14 @@
-import pandas
+import pandas as pd
 
 
+df = pd.read_csv("day1/input.txt", names=["mass"])
+
+
+def get_fuel_required(x):
+    return int(x / 3) - 2
+
+
+df["fuel_required"] = df["mass"].apply(lambda x: get_fuel_required(x))
+
+total_fuel_required = df["fuel_required"].sum()
+print(total_fuel_required)
